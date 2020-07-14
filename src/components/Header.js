145541cf67476e38d013c2caf23c6f2892c9518f
otompo <abagnasalifusasco@gmail.com>
2020-07-14 12:27:39 +0100

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header(props){
+
+
+    return(
+        <div>
+               <nav>
+                    <h1>React Meals</h1>
+                    <a href="">Order</a>
+                    <a href="">Home</a>
+                    <a href="">Contact Us</a>
+               </nav>
+        
+        </div>
+    )
+}
+
+export default Header
