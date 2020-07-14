fcca3a209d72ad4d8f69551bbcf9f27c39b94f5b
otompo <abagnasalifusasco@gmail.com>
2020-07-14 12:27:36 +0100

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Footer(props){
+
+
+    return(
+        <div className="footer">
+               <p>Designed and Developed By</p>
+                <p> SASCO ONE GH</p>
+        </div>
+    )
+}
+
+export default Footer
